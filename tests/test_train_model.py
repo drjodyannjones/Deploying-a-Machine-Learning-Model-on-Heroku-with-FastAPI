8830afd91f41fa49109169ffca8a410415221e46
Jody-Ann Jones <drjodyannjones@Jody-Anns-MBP.lan>
2023-03-19 14:03:08 -0500

    Update test_train_model.py

--- tests/test_train_model.py
+++ tests/test_train_model.py
@@ -1,77 +1,17 @@
 import os
-import joblib
 import pandas as pd
+import joblib
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from starter.ml.data import process_data
 from starter.ml.model import train_model, compute_model_metrics, inference
-from sklearn.preprocessing import LabelBinarizer, LabelEncoder
-
-
-def test_train_model():
-    X_train = pd.DataFrame({
-        "age": [25, 45],
-        "workclass": ["Private", "Self-emp-not-inc"],
-        "education": ["Bachelors", "HS-grad"],
-        "marital-status": ["Never-married", "Married-civ-spouse"],
-        "occupation": ["Adm-clerical", "Exec-managerial"],
-        "relationship": ["Not-in-family", "Husband"],
-        "race": ["White", "Black"],
-        "sex": ["Male", "Female"],
-        "native-country": ["United-States", "Mexico"],
-    })
-    y_train = pd.Series([">50K", "<=50K"])
-
-    model = train_model(X_train, y_train)
-    assert model is not None, "Model not returned"
-
-
-def test_compute_model_metrics():
-    y = pd.Series([1, 0, 1, 0])
-    preds = pd.Series([1, 0, 0, 1])
-
-    precision, recall, fbeta = compute_model_metrics(y, preds)
-
-    assert precision == 0.5, "Incorrect precision"
-    assert recall == 0.5, "Incorrect recall"
-    assert fbeta == 0.5, "Incorrect F1 score"
-
-
-def test_inference():
-    X = pd.DataFrame({
-        "age": [25, 45],
-        "workclass": ["Private", "Self-emp-not-inc"],
-        "education": ["Bachelors", "HS-grad"],
-        "marital-status": ["Never-married", "Married-civ-spouse"],
-        "occupation": ["Adm-clerical", "Exec-managerial"],
-        "relationship": ["Not-in-family", "Husband"],
-        "race": ["White", "Black"],
-        "sex": ["Male", "Female"],
-        "native-country": ["United-States", "Mexico"],
-    })
-    y = pd.Series([">50K", "<=50K"])
-    model = train_model(X, y)
-    preds = inference(model, X)
-
-    assert len(preds) == 2, "Incorrect number of predictions"
-    assert all(isinstance(p, int) for p in preds), "Predictions should be integers"
-
+from sklearn.preprocessing import OneHotEncoder, LabelBinarizer
 
 def test_process_data():
     # Load sample data for testing
-    data = pd.DataFrame({
-        "age": [25, 45],
-        "workclass": ["Private", "Self-emp-not-inc"],
-        "education": ["Bachelors", "HS-grad"],
-        "marital-status": ["Never-married", "Married-civ-spouse"],
-        "occupation": ["Adm-clerical", "Exec-managerial"],
-        "relationship": ["Not-in-family", "Husband"],
-        "race": ["White", "Black"],
-        "sex": ["Male", "Female"],
-        "native-country": ["United-States", "Mexico"],
-        "salary": [">50K", "<=50K"]
-    })
+    data = pd.read_csv("data/census.csv")
+    train, test = train_test_split(data, test_size=0.20)
 
-    # Define categorical features
     cat_features = [
         "workclass",
         "education",
@@ -83,5 +23,88 @@ def test_process_data():
         "native-country",
     ]
 
-    X_train, y_train, encoder, lb, scaler = process_data(
-        data, categorical_features
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+    assert X_train is not None, "X_train not returned"
+    assert y_train is not None, "y_train not returned"
+    assert encoder is not None, "encoder not returned"
+    assert lb is not None, "lb not returned"
+
+def test_train_model():
+    data = pd.read_csv("data/census.csv")
+    train, test = train_test_split(data, test_size=0.20)
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+    model = train_model(X_train, y_train)
+    assert model is not None, "Model not returned"
+
+def test_inference():
+    data = pd.read_csv("data/census.csv")
+    train, test = train_test_split(data, test_size=0.20)
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+    X_test, y_test, _, _ = process_data(
+        test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+    )
+    model = train_model(X_train, y_train)
+    preds = inference(model, X_test)
+
+    assert preds is not None, "Predictions not returned"
+    assert len(preds) == len(y_test), "Incorrect number of predictions"
+
+def test_compute_model_metrics():
+    data = pd.read_csv("data/census.csv")
+    train, test = train_test_split(data, test_size=0.20)
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+    X_test, y_test, _, _ = process_data(
+        test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+    )
+    model = train_model(X_train, y_train)
+    preds = inference(model, X_test)
+
+    precision, recall, fbeta = compute_model_metrics(y_test, preds)
+
+    assert precision is not None, "Precision not returned"
+    assert recall is not None, "Recall not returned"
+    assert fbeta is not None, "Fbeta not returned"
