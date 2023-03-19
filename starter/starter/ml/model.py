d8e825515a9557eccfce9b05b497841dfdbe6da4
Jody-Ann Jones <drjodyannjones@Jody-Anns-MBP.lan>
2023-03-19 12:47:51 -0500

    Update model.py

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
