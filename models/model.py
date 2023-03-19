0b88f76aaa76218b844045bfa533a860c6de5c87
Jody-Ann Jones <drjodyannjones@Jody-Anns-MBP.lan>
2023-03-19 17:03:38 -0500

    Update model.py

--- models/model.py
+++ models/model.py
@@ -1,94 +1,35 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.metrics import f1_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 
-def train_model(X_train, y_train):
-    """
-    Trains a machine learning model and returns it.
-
-    Inputs
-    ------
-    X_train : np.array
-        Training data.
-    y_train : np.array
-        Labels.
-    Returns
-    -------
-    model
-        Trained machine learning model.
-    """
-
-    # Initialize the RandomForestClassifier
+def train_model(X_train: np.array, y_train: np.array) -> RandomForestClassifier:
     model = RandomForestClassifier(n_estimators=100, random_state=42)
-
-    # Train the model using the training data and labels
     model.fit(X_train, y_train)
-
     return model
 
-
-def compute_model_metrics(y, preds):
-    """
-    Validates the trained machine learning model using precision, recall, and F1.
-
-    Inputs
-    ------
-    y : np.array
-        Known labels, binarized.
-    preds : np.array
-        Predicted labels, binarized.
-    Returns
-    -------
-    precision : float
-    recall : float
-    fbeta : float
-    """
-    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
+def compute_model_metrics(y: np.array, preds: np.array) -> tuple:
+    f1 = f1_score(y, preds, zero_division=1)
     precision = precision_score(y, preds, zero_division=1)
     recall = recall_score(y, preds, zero_division=1)
-    return precision, recall, fbeta
+    return precision, recall, f1
 
-
-def inference(model, X):
-    """ Run model inferences and return the predictions.
-
-    Inputs
-    ------
-    model : ???
-        Trained machine learning model.
-    X : np.array
-        Data used for prediction.
-    Returns
-    -------
-    preds : np.array
-        Predictions from the model.
-    """
-
-    # Use the trained model to make predictions on the input data
+def inference(model: RandomForestClassifier, X: np.array) -> np.array:
     preds = model.predict(X)
-
     return preds
 
-
 if __name__ == '__main__':
-    # Load your data and split it into X_train, y_train, X_test, and y_test here
     data = pd.read_csv("data/census.csv", delimiter=",")
     X = data.iloc[:, :-1].values
     y = data.iloc[:, -1].values
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Train the model
     model = train_model(X_train, y_train)
-
-    # Run inference on the test data
     preds = inference(model, X_test)
-
-    # Compute model metrics
-    precision, recall, fbeta = compute_model_metrics(y_test, preds)
+    precision, recall, f1 = compute_model_metrics(y_test, preds)
 
     print(f"Precision: {precision}")
     print(f"Recall: {recall}")
-    print(f"F1 Score: {fbeta}")
+    print(f"F1 Score: {f1}")
