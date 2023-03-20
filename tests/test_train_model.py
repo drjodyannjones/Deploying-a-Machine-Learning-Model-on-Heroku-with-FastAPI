79c8761406acf098947b29542ad41d9f9e79a574
Jody-Ann Jones <drjodyannjones@Jody-Anns-MBP.lan>
2023-03-20 18:09:23 -0500

    Update test_train_model.py

--- tests/test_train_model.py
+++ tests/test_train_model.py
@@ -1,56 +1,56 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_recall_fscore_support
+import joblib
 import sys
+import os
+
+# Update the sys.path to include the 'src' directory
 sys.path.append('src')
 
-import os
-import pandas as pd
-import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from app.models.train_model import process_data, train_model, compute_model_metrics, inference
-from sklearn.preprocessing import OneHotEncoder, LabelBinarizer
+# Import necessary functions from the main script
+from test_train_model import train_model, inference, compute_model_metrics, process_data
 
-def test_compute_model_metrics():
-    data_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'census.csv')
-    data = pd.read_csv(data_path, delimiter=',', engine='python')
+# Load the data
+data_path = 'data/census.csv'
+data = pd.read_csv(data_path)
 
-    # Remove extra spaces from the column names and data
-    # data.columns = data.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
-    # data = data.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+# Define categorical features
+cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
 
-    train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(data, test_size=0.20)
 
-    cat_features = [
-        "workclass",
-        "education",
-        "marital_status",
-        "occupation",
-        "relationship",
-        "race",
-        "sex",
-        "native_country",
-    ]
+# Process the data
+X_train, y_train, ct, lb = process_data(
+    train, categorical_features=cat_features, label="salary", training=True
+)
 
-    X_train, y_train, encoder, lb = process_data(
-        train, categorical_features=cat_features, label="salary", training=True
-    )
-    X_test, y_test, _, _ = process_data(
-        test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
-    )
-    model = train_model(X_train, y_train)
-    preds = inference(model, X_test)
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, ct=ct, lb=lb
+)
 
-    precision, recall, fbeta = compute_model_metrics(y_test, preds)
+# Train the model
+model = train_model(X_train, y_train)
 
-    assert precision is not None, "Precision not returned"
-    assert recall is not None, "Recall not returned"
-    assert fbeta is not None, "Fbeta not returned"
+# Perform inference
+preds = inference(model, X_test)
 
-    # Check the type of the computed metrics
-    assert isinstance(precision, float), "Precision is not a float"
-    assert isinstance(recall, float), "Recall is not a float"
-    assert isinstance(fbeta, float), "Fbeta is not a float"
+# Compute model metrics
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
 
-    # Check the range of the computed metrics
-    assert precision >= 0 and precision <= 1, "Precision is not within [0, 1]"
-    assert recall >= 0 and recall <= 1, "Recall is not within [0, 1]"
-    assert fbeta >= 0 and fbeta <= 1, "Fbeta is not within [0, 1]"
+# Test assertions
+assert 0 <= precision <= 1, "Precision should be between 0 and 1"
+assert 0 <= recall <= 1, "Recall should be between 0 and 1"
+assert 0 <= fbeta <= 1, "F1 Score should be between 0 and 1"
+
+print("All tests passed!")
