77efdaacd1abdd484a2bb203d51009d1e664c537
Jody-Ann Jones <drjodyannjones@Jody-Anns-MBP.lan>
2023-03-20 18:01:17 -0500

    Update train_model.py

--- src/app/models/train_model.py
+++ src/app/models/train_model.py
@@ -1,4 +1,3 @@
-# Script to train machine learning model.
 import sys
 sys.path.append('src')
 
@@ -6,11 +5,32 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from app.models.data import process_data
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.metrics import precision_recall_fscore_support
 import joblib
 import os
+from typing import List
 
-from sklearn.metrics import precision_recall_fscore_support
+
+class CustomTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.imputer = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+        self.encoder = OneHotEncoder(handle_unknown='ignore')
+
+    def fit(self, X, y=None):
+        self.imputer.fit(X)
+        X_imputed = self.imputer.transform(X)
+        self.encoder.fit(X_imputed)
+        return self
+
+    def transform(self, X, y=None):
+        X_imputed = self.imputer.transform(X)
+        X_encoded = self.encoder.transform(X_imputed)
+        return X_encoded
 
 def train_model(X_train, y_train):
     model = RandomForestClassifier(random_state=42)
@@ -29,8 +49,7 @@ def compute_model_metrics(y_true, y_pred):
 data_path = 'data/census.csv'  # Replace with the path to your data file
 data = pd.read_csv(data_path)
 
-train, test = train_test_split(data, test_size=0.20)
-
+# Define cat_features list
 cat_features = [
     "workclass",
     "education",
@@ -41,27 +60,46 @@ cat_features = [
     "sex",
     "native-country",
 ]
-X_train, y_train, encoder, lb, scaler = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
+
+train, test = train_test_split(data, test_size=0.20)
+
+# Encode the target variable
+lb = LabelEncoder()
+train['salary'] = lb.fit_transform(train['salary'])
+test['salary'] = lb.transform(test['salary'])
+
+# Define column transformer
+ct = ColumnTransformer(
+    transformers=[('cat', CustomTransformer(), cat_features)],
+    remainder='passthrough'
 )
 
-encoder_path = 'src/app/models/encoder.pkl'
-joblib.dump(encoder, encoder_path)
+# Fit and transform the training data
+X_train = ct.fit_transform(train.drop('salary', axis=1))
+y_train = train['salary'].values
+
+# Transform the test data
+X_test = ct.transform(test.drop('salary', axis=1))
+y_test = test['salary'].values
+
+# Train the model
+model = train_model(X_train, y_train)
+
+# Save the column transformer and label encoder
+ct_path = 'src/app/models/column_transformer.pkl'
+joblib.dump(ct, ct_path)
 
 lb_path = 'src/app/models/label_binarizer.pkl'
 joblib.dump(lb, lb_path)
 
-X_test, y_test, _, _, _ = process_data(
-    test, categorical_features=cat_features, label="salary", training=False, le=encoder, lb=lb
-)
-
-model = train_model(X_train, y_train)
-
+# Save the trained model
 model_path = 'src/app/models/model.pkl'  # Updated model path
 joblib.dump(model, model_path)
 
+# Inference
 preds = inference(model, X_test)
 
+# Compute model metrics
 precision, recall, fbeta = compute_model_metrics(y_test, preds)
 
 print(f"Precision: {precision}")
