b2f529c308a3849bb0dc264c4e4ef72bd0f12158
Jody-Ann Jones <drjodyannjones@Jody-Anns-MacBook-Pro.local>
2023-03-21 12:01:57 -0500

    Update repo

--- tests/test_model.py
+++ tests/test_model.py
@@ -28,4 +28,5 @@ def test_inference():
     preds = inference(model, X)
 
     assert len(preds) == 2, "Incorrect number of predictions"
-    assert all(isinstance(p, int) for p in preds), "Predictions should be integers"
+    assert all(isinstance(p[0], int) for p in preds), "Predictions should be integers"
+
