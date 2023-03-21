5a6289c5c3de39dabb8f84841d841acc34408fb6
Jody-Ann Jones <drjodyannjones@Jody-Anns-MacBook-Pro.local>
2023-03-21 13:06:01 -0500

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,14 +2,12 @@ import numpy as np
 import pandas as pd
 from src.app.models.model import train_model, compute_model_metrics, inference
 
-
 def test_train_model():
     X_train = np.array([[1, 2], [3, 4]])
     y_train = np.array([0, 1])
 
     model = train_model(X_train, y_train)
     assert model is not None, "Model not returned"
-
 
 def test_compute_model_metrics():
     y = np.array([1, 0, 1, 0])
@@ -21,12 +19,10 @@ def test_compute_model_metrics():
     assert recall == 0.5, "Incorrect recall"
     assert fbeta == 0.5, "Incorrect F1 score"
 
-
 def test_inference():
     X = np.array([[1, 2], [3, 4]])
     model = train_model(X, np.array([0, 1]))
     preds = inference(model, X)
 
     assert len(preds) == 2, "Incorrect number of predictions"
-    assert all(isinstance(p[0], int) for p in preds), "Predictions should be integers"
-
+    assert all(isinstance(p, int) for p in preds), "Predictions should be integers"
