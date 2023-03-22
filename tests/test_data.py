f821e65b2e2c45e0179a6f1c824c590cb702f308
Jody-Ann Jones <drjodyannjones@Jody-Anns-MBP.lan>
2023-03-22 11:21:15 -0500

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -36,12 +36,12 @@ def test_process_data():
     ]
 
     X_train, y_train, encoder, lb, scaler, output_columns = process_data(
-        data, categorical_features=cat_features, label="None", training=True)
+        data, categorical_features=cat_features, label="salary", training=True)
 
     y_train = y_train.ravel()  # Add this line to make y_train 1-dimensional
 
     assert X_train.shape == (1, 17), "Processed data shape mismatch"
-    assert y_train.shape == (2,), "Labels shape mismatch"
+    assert y_train.shape == (1,), "Labels shape mismatch"
     assert encoder is not None, "Encoder not returned"
     assert lb is not None, "Label Binarizer not returned"
     assert output_columns is not None, "Output columns not returned"
