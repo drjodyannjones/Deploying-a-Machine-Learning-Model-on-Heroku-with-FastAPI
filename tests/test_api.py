184c10b212befb84c93c3b3215cd88f74e25c0d0
Jody-Ann Jones <drjodyannjones@Jody-Anns-MBP.lan>
2023-03-21 20:32:33 -0500

    Update tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -17,7 +17,7 @@ def test_predict_above_50k() -> None:
         "fnlwgt": 121772,
         "education": "Assoc-voc",
         "education_num": 11,
-        "marital_status": "Married-civ-spouse",
+        "marital-status": "Married-civ-spouse",
         "occupation": "Craft-repair",
         "relationship": "Husband",
         "race": "Asian-Pac-Islander",
@@ -25,7 +25,7 @@ def test_predict_above_50k() -> None:
         "capital_gain": 7298,
         "capital_loss": 0,
         "hours_per_week": 40,
-        "native_country": "United-States"
+        "native-country": "United-States"
     }
     response = client.post("/predict", json=input_data)
     print(response.content)
@@ -39,7 +39,7 @@ def test_predict_below_50k() -> None:
         "fnlwgt": 226802,
         "education": "11th",
         "education_num": 7,
-        "marital_status": "Never-married",
+        "marital-status": "Never-married",
         "occupation": "Machine-op-inspct",
         "relationship": "Own-child",
         "race": "Black",
@@ -47,7 +47,7 @@ def test_predict_below_50k() -> None:
         "capital_gain": 0,
         "capital_loss": 0,
         "hours_per_week": 40,
-        "native_country": "United-States"
+        "native-country": "United-States"
     }
     response = client.post("/predict", json=input_data)
     print(response.content)
