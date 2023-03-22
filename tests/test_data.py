8aed577f07722e7a40aae8fec3c0b09b8638ed0b
Jody-Ann Jones <drjodyannjones@Jody-Anns-MBP.lan>
2023-03-22 11:13:07 -0500

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,21 +6,22 @@ from src.app.models.data import process_data
 def test_process_data():
     # Load sample data for testing
     data = pd.DataFrame({
-        "age": 40,
-        "workclass": "Private",
-        "fnlwgt": 121772,
-        "education": "Assoc-voc",
-        "education_num": 11,
-        "marital_status": "Married-civ-spouse",
-        "occupation": "Craft-repair",
-        "relationship": "Husband",
-        "race": "Asian-Pac-Islander",
-        "sex": "Male",
-        "capital_gain": 7298,
-        "capital_loss": 0,
-        "hours_per_week": 40,
-        "native_country": "United-States"
-    })
+    "age": [40],
+    "workclass": ["Private"],
+    "fnlwgt": [121772],
+    "education": ["Assoc-voc"],
+    "education_num": [11],
+    "marital_status": ["Married-civ-spouse"],
+    "occupation": ["Craft-repair"],
+    "relationship": ["Husband"],
+    "race": ["Asian-Pac-Islander"],
+    "sex": ["Male"],
+    "capital_gain": [7298],
+    "capital_loss": [0],
+    "hours_per_week": [40],
+    "native_country": ["United-States"]
+})
+
 
     # Define categorical features
     cat_features = [
@@ -39,7 +40,7 @@ def test_process_data():
 
     y_train = y_train.ravel()  # Add this line to make y_train 1-dimensional
 
-    assert X_train.shape == (2, 17), "Processed data shape mismatch"
+    assert X_train.shape == (1, 17), "Processed data shape mismatch"
     assert y_train.shape == (2,), "Labels shape mismatch"
     assert encoder is not None, "Encoder not returned"
     assert lb is not None, "Label Binarizer not returned"
