f768b668b701840265dd9b07e7a28d3a12ea7dcd
Jody-Ann Jones <drjodyannjones@Jody-Anns-MBP.lan>
2023-03-22 13:29:25 -0500

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -8,22 +8,23 @@ from app.main import scaler
 def test_process_data():
     # Load sample data for testing
     data = pd.DataFrame({
-    "age": [40],
-    "workclass": ["Private"],
-    "fnlgt": [121772],
-    "education": ["Assoc-voc"],
-    "education_num": [11],
-    "marital_status": ["Married-civ-spouse"],
-    "occupation": ["Craft-repair"],
-    "relationship": ["Husband"],
-    "race": ["Asian-Pac-Islander"],
-    "sex": ["Male"],
-    "capital_gain": [7298],
-    "capital_loss": [0],
-    "hours_per_week": [40],
-    "native_country": ["United-States"],
-    "salary": [">50K"]
+    "age": [40, 25],
+    "workclass": ["Private", "Self-emp-not-inc"],
+    "fnlgt": [121772, 226802],
+    "education": ["Assoc-voc", "11th"],
+    "education_num": [11, 7],
+    "marital_status": ["Married-civ-spouse", "Never-married"],
+    "occupation": ["Craft-repair", "Machine-op-inspct"],
+    "relationship": ["Husband", "Own-child"],
+    "race": ["Asian-Pac-Islander", "Black"],
+    "sex": ["Male", "Female"],
+    "capital_gain": [7298, 0],
+    "capital_loss": [0, 0],
+    "hours_per_week": [40, 40],
+    "native_country": ["United-States", "United-States"],
+    "salary": [">50K", "<=50K"]
 })
+
 
 
     # Define categorical features
@@ -43,7 +44,7 @@ def test_process_data():
 
     y_train = y_train.ravel()  # Add this line to make y_train 1-dimensional
 
-    assert X_train.shape == (1, 17), "Processed data shape mismatch"
+    assert X_train.shape == (2, 17), "Processed data shape mismatch"
     assert y_train.shape == (1,), "Labels shape mismatch"
     assert encoder is not None, "Encoder not returned"
     assert lb is not None, "Label Binarizer not returned"
