ec086f2997dd8f40a3ab91824f5f1ad60ec7fa09
Jody-Ann Jones <drjodyannjones@Jody-Anns-MBP.lan>
2023-03-23 11:28:14 -0500

    Update repo

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,18 +4,14 @@ from typing import Dict
 import pandas as pd
 import numpy as np
 from fastapi.testclient import TestClient
-import joblib
 
 # Add the path to the src folder to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
 
 from app.main import app
-from app.main import scaler, encoder, lb
+from app.main import scaler, encoder, lb, model
 
 client = TestClient(app)
-
-# Load the serialized model
-model = joblib.load(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src", "app", "models", "model.pkl")))
 
 def test_welcome() -> None:
     response = client.get("/")
@@ -40,18 +36,14 @@ def test_predict_above_50k() -> None:
         "native_country": "United-States"
     }
 
-    # Encode categorical variables
-    encoded_data = encoder.transform(pd.DataFrame(input_data, index=[0]))
+    input_df = pd.DataFrame(input_data, index=[0])
 
     # Scale numerical variables
-    scaled_data = scaler.transform(encoded_data)
+    scaled_data = scaler.transform(input_df)
 
-    # Make prediction
     prediction = lb.inverse_transform(model.predict(scaled_data))[0]
 
-    response = client.post("/predict", json=input_data)
-    assert response.status_code == 200
-    assert response.json() == {"prediction": prediction}
+    assert prediction == ">50K"
 
 def test_predict_below_50k() -> None:
     input_data = {
@@ -71,15 +63,11 @@ def test_predict_below_50k() -> None:
         "native_country": "United-States"
     }
 
-    # Encode categorical variables
-    encoded_data = encoder.transform(pd.DataFrame(input_data, index=[0]))
+    input_df = pd.DataFrame(input_data, index=[0])
 
     # Scale numerical variables
-    scaled_data = scaler.transform(encoded_data)
+    scaled_data = scaler.transform(input_df)
 
-    # Make prediction
     prediction = lb.inverse_transform(model.predict(scaled_data))[0]
 
-    response = client.post("/predict", json=input_data)
-    assert response.status_code == 200
-    assert response.json() == {"prediction": prediction}
+    assert prediction == "<=50K"
